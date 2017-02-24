94a426314539ee524bca9ff4f5e4cdec2c941f2c
Theodor Nolte <theno@gmx.net>
2017-02-24 17:45:06 +0100

    update version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except(IOError, ImportError, RuntimeError):
 
 setup(
     name='utlz',
-    version='0.6.2',
+    version='0.7.0',
     description=description,
     long_description=long_description,
     url='https://github.com/theno/utlz',
